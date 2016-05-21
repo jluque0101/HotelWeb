2e7364cc587e447f08666de3ba8e7cd401e4ab81
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-05-21 06:44:13 +0200

    commented some urls

--- hotel/urls.py
+++ hotel/urls.py
@@ -8,14 +8,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^formset$', DefaultFormsetView.as_view(), name='formset_default'),
-    url(r'^form$', DefaultFormView.as_view(), name='form_default'),
-    url(r'^form_by_field$', DefaultFormByFieldView.as_view(), name='form_by_field'),
-    url(r'^form_horizontal$', FormHorizontalView.as_view(), name='form_horizontal'),
-    url(r'^form_inline$', FormInlineView.as_view(), name='form_inline'),
-    url(r'^form_with_files$', FormWithFilesView.as_view(), name='form_with_files'),
-    url(r'^pagination$', PaginationView.as_view(), name='pagination'),
-    url(r'^misc$', MiscView.as_view(), name='misc'),
+    # url(r'^formset$', DefaultFormsetView.as_view(), name='formset_default'),
+    # url(r'^form$', DefaultFormView.as_view(), name='form_default'),
+    # url(r'^form_by_field$', DefaultFormByFieldView.as_view(), name='form_by_field'),
+    # url(r'^form_horizontal$', FormHorizontalView.as_view(), name='form_horizontal'),
+    # url(r'^form_inline$', FormInlineView.as_view(), name='form_inline'),
+    # url(r'^form_with_files$', FormWithFilesView.as_view(), name='form_with_files'),
+    # url(r'^pagination$', PaginationView.as_view(), name='pagination'),
+    # url(r'^misc$', MiscView.as_view(), name='misc'),
 )
 
 # Development
