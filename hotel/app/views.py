a2cdae77f96af99a1c47d0cb5a7d4ae9c8c69549
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-06-01 21:33:59 +0200

    added contact form

--- hotel/app/views.py
+++ hotel/app/views.py
@@ -48,8 +48,16 @@ class BookingPageView(TemplateView):
 class GalleryPageView(TemplateView):
     template_name = 'gallery.html'
 
-
     def get_context_data(self, **kwargs):
         context = super(GalleryPageView, self).get_context_data(**kwargs)
         context['page'] = 'gallery'
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactPageView, self).get_context_data(**kwargs)
+        context['contact'] = 'contact'
         return context
